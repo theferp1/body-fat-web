211f9d0b5488d0a6a01151b93c936cf552704b70
Nicolas Primo <nicolas.primo@lapisco.ifce.edu.br>
2024-04-06 15:14:26 -0300

    navbar modifications

--- src/layouts/navbar.tsx
+++ src/layouts/navbar.tsx
@@ -1,7 +1,26 @@
+import NavButton from "../components/NavButton";
+import {
+  IconBrandGithubFilled,
+  IconBookFilled,
+  IconMenuDeep,
+} from "@tabler/icons-react";
+
 const Navbar = () => {
-    return (
-        <div className="w-full h-12 bg-white"></div>
-    );
-}
- 
+  const navButtons = [
+    { name: "Github", icon: <IconBrandGithubFilled /> },
+    { name: "Documentation", icon: <IconBookFilled /> },
+    { name: "Examples", icon: <IconMenuDeep /> },
+  ];
+  return (
+    <nav className="flex items-center justify-between w-screen pt-3 pb-3 pl-6 pr-6 bg-transparent h-fit">
+      <img src="/lapiscoLogo.svg" alt="lapisco logo" />
+      <div className="flex justify-between w-[400px] h-fit">
+        {navButtons.map((button) => (
+          <NavButton icon={button.icon}>{button.name}</NavButton>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
