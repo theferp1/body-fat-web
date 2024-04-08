b401220ba35fe18690f6ea3ec4f62e326774d4c2
Nicolas2Primo <nicolasprimo@gmail.com>
2024-04-08 16:41:54 -0300

    feat:Add footer lapicos

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/getstarted" element={<GetStarted />} />
         </Routes>
+        <span className=" fixed z-50 text-[10px]  text-black sm:text-sm text-opacity-70 sm:text-opacity-60 bottom-2 right-3">
+          © Laboratory for Processing Image, Signals and Computer Science
+        </span>
       </Router>
     </div>
   );
